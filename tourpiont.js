0b47fe294651af0d00f9c59eca267955d448c45e
584worakamon <124670752+584worakamon@users.noreply.github.com>
2023-04-25 16:08:54 +0700

    Add files via upload

---
+++ tourpiont.js
@@ -0,0 +1,156 @@
+var tourpoint =
+{
+ "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "BanRachanRetroThaiMarket",
+        "type": "culture",
+        "level": 6
+      },
+      "geometry": {
+        "coordinates": [
+          100.31485141104378,
+          14.887136737419752
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "WatPhikunThong",
+        "type": "culture",
+        "level": 7
+      },
+      "geometry": {
+        "coordinates": [
+          100.1712740293379,
+          14.721641623511971
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "BangRachanHeroes'Monument",
+        "type": "culture",
+        "level": 9
+      },
+      "geometry": {
+        "coordinates": [
+          100.29644972272098,
+          14.896623692039654
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "WatSai",
+        "type": "culture",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.29492517944828,
+          15.06543557844762
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "WatMuangChum",
+        "type": "culture",
+        "level": 7
+      },
+      "geometry": {
+        "coordinates": [
+          100.31727700000175,
+          14.884720000000456
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "WatPhraNonChakSiWorawihan",
+        "type": "culture",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.38848459703343,
+          14.851780379711784
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "WatPhoKaoTon",
+        "type": "culture",
+        "level": 6
+      },
+      "geometry": {
+        "coordinates": [
+          100.32137100000091,
+          14.802397293549333
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "WatPhraPrang",
+        "type": "culture",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.44398327116494,
+          14.86053099999836
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "BanRachanRetroThaiMarket",
+        "type": "market",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.3222767288371,
+          14.800719000000797
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "WatAmphawan",
+        "type": "culture",
+        "level": 10
+      },
+      "geometry": {
+        "coordinates": [
+          100.43950275656209,
+          14.823222085263794
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
